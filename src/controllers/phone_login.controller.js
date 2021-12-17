cfd055ace51d5a716be85d9dcbed9b0f646892a4
pankaj kumar <raipankaj150@gmail.com>
2021-12-17 11:27:28 +0530

    comit initiate

--- src/controllers/phone_login.controller.js
+++ src/controllers/phone_login.controller.js
@@ -8,18 +8,20 @@ router.get("", async (req, res) => {
 });
 
 router.post("", async (req, res) => {
-  const phone_num = req.body.phone_num;
-  const password = req.body.password;
-  const login = await Login.findOne({
-    phone_num: phone_num,
-    password: password,
-  });
-  if (login === null) {
-    console.log("User doesn't exist");
-    res.redirect("/signup");
-  } else {
-    // res.redirect("/home");
-    console.log("login successfull");
-  }
+  // const phone_num = req.body.phone_num;  // req.body = phone number, passswrod if(!user){ res}
+  // const password = req.body.password;
+  // const login = await Login.findOne({
+  //   phone_num: phone_num,
+  //   password: password,
+  // });
+  // if (login === null) {
+  //   console.log("User doesn't exist");
+  //   res.redirect("/signup");
+  // } else {
+  //   // res.redirect("/home");            //password-check->craete token  res.render("/",{user,token})
+  //   console.log("login successfull");
+  // }
+
+  res.render("signup",{message:"register here"});
 });
 module.exports = router;
--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+
+
 const Signup = require("../models/signup.model");
 
 router.get("", async (req, res) => {
@@ -8,9 +10,47 @@ router.get("", async (req, res) => {
 });
 
 router.post("", async (req, res) => {
-  const signup = await Signup.create(req.body);
-  res.redirect("/email_login");
-  res.redirect("/phone_login");
+  try {
+    let user = await Signup.findOne({email:req.body.email}).lean().exec();
+
+    if(user) return res.status(500).render("signup",{ message:"user exist"});
+
+    user = await Signup.create(req.body);
+    // const token = newToken(user);
+
+    return res.render("signup")
+} catch (e) {
+    res.status(500).render("signup",{ message:"REGISTRATION FAILED"})
+}
+
 });
+
+// router.post("", 
+// body("phone_num").isLength({min: 10, max:10}).withMessage("phonenumber is required and has to be 10 digit"),
+// body("email").isEmail().withMessage("email is required"),
+// body("password").isLength({min:7}).withMessage("password is required and must be at least 7 characters"),
+// body("name").isLength({min:1}).withMessage("name is required")
+// ,async (req, res) => {
+//   const errors = validationResult(req);
+//     if(!errors.isEmpty()){
+//         let a=errors.array().map(({msg,param,location})=>{
+//             return {
+//                 msg,
+//             }
+//         })
+//         return res.status(400).send({errors: a});
+//     }
+  
+//     let find = await Signup.findOne({ email: req.body.email })
+//     if (find) {
+//       console.log("find");
+//       return res.status(201).send({find});
+//     } else {
+//       const signup = await Signup.create(req.body);
+//       console.log("posted");
+//       return res.status(200).send({signup})
+//       // res.redirect("/email_login");
+//       // res.redirect("/phone_login");
+//     }
 
 module.exports = router;
