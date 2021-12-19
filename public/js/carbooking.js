7ac0245fde8df1a628dd70944d098709e55b0587
pankaj kumar <raipankaj150@gmail.com>
2021-12-19 11:34:26 +0530

    title

--- public/js/carbooking.js
+++ public/js/carbooking.js
@@ -64,7 +64,10 @@ async function nintyfun() {
 }
 
 async function onetwontyfun() {
+  
     let data = await fetch("http://localhost:4500/cars");
+
+
 
     data = await data.json();
 
@@ -86,11 +89,12 @@ let sevenSeater = [];
 
 // five seater filter function
 async function five() {
+
     fiveSeater.innerHTML = null
     let data = await fetch("http://localhost:4500/cars");
 
     data = await data.json();
-
+    console.log(data);
     let cars = data[city];
 
     cars.forEach((e) => {
@@ -300,66 +304,66 @@ function hideSideBar() {
 
 // user logged in or logged out
 
-if(JSON.parse(localStorage.getItem("user"))!==null){
+// if(JSON.parse(localStorage.getItem("user"))!==null){
 
-    let user_data = JSON.parse(localStorage.getItem("user"));
-    console.log(user_data[0].name)
+//     let user_data = JSON.parse(localStorage.getItem("user"));
+//     console.log(user_data[0].name)
     
-    let user = document.getElementById("user");
-    user.style.display = "flex";
-    user.style.alignItems = "center";
+//     let user = document.getElementById("user");
+//     user.style.display = "flex";
+//     user.style.alignItems = "center";
 
-    let nav_name = document.getElementById("nav_name");
-    nav_name.innerText = user_data[0].name;
+//     let nav_name = document.getElementById("nav_name");
+//     nav_name.innerText = user_data[0].name;
 
 
-    let side_name = document.getElementById("side_name");
-    side_name.innerText = user_data[0].name;
+//     let side_name = document.getElementById("side_name");
+//     side_name.innerText = user_data[0].name;
 
-    let side_email = document.getElementById("side_email");
-    side_email.innerText = user_data[0].email
+//     let side_email = document.getElementById("side_email");
+//     side_email.innerText = user_data[0].email
 
-    let side_phone = document.getElementById("side_phone");
-    side_phone.innerText = user_data[0].mobile;
+//     let side_phone = document.getElementById("side_phone");
+//     side_phone.innerText = user_data[0].mobile;
 
-    let user_not_login = document.getElementById("user_not_login");
-    user_not_login.style.display = "none";
+//     let user_not_login = document.getElementById("user_not_login");
+//     user_not_login.style.display = "none";
 
-    let not_logged = document.querySelector(".not-loggedin");
-    not_logged.style.display="none";
+//     let not_logged = document.querySelector(".not-loggedin");
+//     not_logged.style.display="none";
 
     
-    let user_looged_in = document.querySelector(".user-logged-in");
-    user_looged_in.style.display="block";
+//     let user_looged_in = document.querySelector(".user-logged-in");
+//     user_looged_in.style.display="block";
 
-    let logout = document.querySelector(".logout");
-    logout.style.display = "block";
-
-
-}else{
-    let user = document.getElementById("user");
-    user.style.display = "none";
-
-    let user_not_login = document.getElementById("user_not_login");
-    user_not_login.style.display = "block";
-
-    let not_logged = document.querySelector(".not-loggedin");
-    not_logged.style.display="flex";
+//     let logout = document.querySelector(".logout");
+//     logout.style.display = "block";
 
 
-    let user_looged_in = document.querySelector(".user-logged-in");
-    user_looged_in.style.display="none";
+// }else{
+//     let user = document.getElementById("user");
+//     user.style.display = "none";
 
-    pick_up.innerText = "Pick Up City, Airport, Address or Hotel";
-    drop_off.innerText = "Drop Off City, Airport, Address or Hotel";
+//     let user_not_login = document.getElementById("user_not_login");
+//     user_not_login.style.display = "block";
+
+//     let not_logged = document.querySelector(".not-loggedin");
+//     not_logged.style.display="flex";
 
 
-    let logout = document.querySelector(".logout");
-    logout.style.display = "none";
-}
+//     let user_looged_in = document.querySelector(".user-logged-in");
+//     user_looged_in.style.display="none";
+
+//     pick_up.innerText = "Pick Up City, Airport, Address or Hotel";
+//     drop_off.innerText = "Drop Off City, Airport, Address or Hotel";
 
 
-function logout(){
-    localStorage.setItem("user",null)
-    window.location.reload()
-}
+//     let logout = document.querySelector(".logout");
+//     logout.style.display = "none";
+// }
+
+
+// function logout(){
+//     localStorage.setItem("user",null)
+//     window.location.reload()
+// }
